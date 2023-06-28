22f2cae72261c14190ec3ad35ac5aa387c698e98
Martijn Dierckx <martijn@smard.be>
2023-06-28 22:34:58 +0200

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 import { GardenaConnection } from './GardenaConnection';
 export default GardenaConnection;
+
+export type * from './GardenaAccessToken';
+export type * from './GardenaAuth';
+export type * from './GardenaDevice';
+export type * from './GardenaLocation';
+export type * from './GardenaMower';
