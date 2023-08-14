19d21ab15575a8c58af822c71127ee96c7df08d2
Martijn Dierckx <martijn@smard.be>
2023-08-14 12:28:38 +0200

    proper export

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ export default GardenaConnection;
 
 export type * from './GardenaAccessToken';
 export type * from './GardenaAuth';
-export type * from './GardenaDevice';
-export type * from './GardenaLocation';
-export type * from './GardenaMower';
+
+export * from './GardenaDevice.js';
+export * from './GardenaLocation.js';
+export * from './GardenaMower.js';
 
 export * from './Enums.js';
 
 export { GardenaWrongCredentialsError, GardenaAuthError } from './GardenaAuth.js';
 export { GardenaApiError } from './GardenaConnection.js';
-export { GardenaLocationError } from './GardenaLocation.js'
+//export { GardenaLocationError } from './GardenaLocation.js'
